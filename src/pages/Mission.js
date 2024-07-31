e8c398fd40e3d46e1527b678a739e537e8cd6816
pookey1104 <pookey1104@naver.com>
2024-08-01 07:14:35 +0900

    Feat : 헤더 구현 (#4)

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -2,14 +2,21 @@ import React from "react";
 import * as C from "../styles/CommonStyle";
 import * as M from "../styles/MissionStyle";
 
+import Header from "../components/header";
+
 function Mission() {
+  const mainTitle = "매일한방 매일습관";
+  const subTitle = "나의 체질에 맞는 건강 습관 만들기";
+
   return (
     <>
       <C.Page>
         <C.Center>
           <M.Background>
             <C.PageSpace>
-              <M.Mission></M.Mission>
+              <M.Mission>
+                <Header mainTitle={mainTitle} subTitle={subTitle} />
+              </M.Mission>
             </C.PageSpace>
           </M.Background>
         </C.Center>
