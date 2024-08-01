0f7b6bd511cb76ba953e2ed26a6e4bdc2b7934b6
jiwonee02 <nepthun@naver.com>
2024-08-01 23:27:29 +0900

    Feat: ai 구현 (#3)

--- src/pages/AI.js
+++ src/pages/AI.js
@@ -5,24 +5,54 @@ import * as A from "../styles/AIStyle";
 import Header from "../components/header";
 
 function AI() {
-  const mainTitle = "사상체질 건강관리";
-  const subTitle = "AI 허준과 함께하는 매일 한방";
+    const mainTitle = "사상체질 건강관리";
+    const subTitle = "AI 허준과 함께하는 매일 한방";
 
-  return (
-    <>
-      <C.Page>
-        <C.Center>
-          <A.Background>
-            <C.PageSpace>
-              <A.AI>
-                <Header mainTitle={mainTitle} subTitle={subTitle} />
-              </A.AI>
-            </C.PageSpace>
-          </A.Background>
-        </C.Center>
-      </C.Page>
-    </>
-  );
+    return (
+        <>
+            <C.Page>
+                <C.Center>
+                    <A.Background>
+                        <C.PageSpace>
+                            <A.AI>
+                                <Header mainTitle={mainTitle} subTitle={subTitle} />
+                                <A.container>
+                                    <A.title>체질별 모아보기</A.title>
+                                    <A.content>
+                                        <A.btn>태양인</A.btn>
+                                        <A.btn>소양인</A.btn>
+                                        <A.btn>태음인</A.btn>
+                                        <A.btn>소음인</A.btn>
+                                    </A.content>
+                                </A.container>
+                                <A.search>
+                                    <A.search_icon>A</A.search_icon>
+                                    <A.input placeholder="태양인 건강 관리법을 알려줘" type="text" name="a"></A.input>
+                                </A.search>
+                                <A.tip>
+                                    <A.tip_text>Tip. 이렇게 물어보면 좋아요</A.tip_text>
+                                    <br></br>
+                                    <A.tip_text> ∙ 건강관리법을 알려줘</A.tip_text>
+                                    <A.tip_text> ∙ 피해야 하는 음식을 알려줘</A.tip_text>
+                                    <A.tip_text> ∙ 치명적인 병을 알려줘</A.tip_text>
+                                    <A.tip_text> ∙ 조심해야하는 생활습관을 알려줘</A.tip_text>
+                                </A.tip>
+                                <A.answer>
+                                    <A.doctor></A.doctor>
+                                    <A.answer_container>
+                                        <A.answer_text>
+                                            태양인의 건강 관리법을 알려드릴게요. 태양인은 발이 다치기 않도록
+                                            조심해야해요.
+                                        </A.answer_text>
+                                    </A.answer_container>
+                                </A.answer>
+                            </A.AI>
+                        </C.PageSpace>
+                    </A.Background>
+                </C.Center>
+            </C.Page>
+        </>
+    );
 }
 
 export default AI;
--- src/styles/AIStyle.js
+++ src/styles/AIStyle.js
@@ -1,7 +1,127 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-  background-color: #28272a;
+    background-color: #28272a;
 `;
 
-export const AI = styled.div``;
+export const AI = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const back = styled.img``;
+
+export const container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 312px;
+    height: 105px;
+    border-radius: 20px;
+    border: 2px solid #f5f5f5;
+    margin-top: 16px;
+`;
+
+export const title = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 106px;
+    height: 24px;
+    font-weight: 600;
+    font-size: 10px;
+    line-height: 11.93px;
+    color: #f5f5f5;
+    margin-top: -1.6rem;
+    box-sizing: borderbox;
+    background-color: #28272a;
+`;
+
+export const content = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const btn = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 60px;
+    height: 60px;
+    border-radius: 20px;
+    background-color: #f5f5f5;
+    border: none;
+    margin: 16px 8px 0 8px;
+    box-sizing: borderbox;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14.32px;
+    color: #3a2929;
+`;
+
+export const search = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 312px;
+    height: 32px;
+    box-sizing: border-box;
+    border-radius: 20px;
+    border: 2px solid #f5f5f5;
+    padding: 4px 18px;
+    margin-top: 36px;
+`;
+
+export const search_icon = styled.label`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 16px;
+    height: 16px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14.32px;
+    color: #f5f5f5;
+`;
+
+export const input = styled.input`
+    display: flex;
+    text-align: center;
+    width: 241.02px;
+    height: 24px;
+    box-sizing: border-box;
+    border: none;
+    background-color: transparent;
+    &::placeholder {
+        text-align: center;
+        font-weight: 600;
+        font-size: 12px;
+        line-height: 14.32px;
+        color: #f5f5f5;
+    }
+    color: #f5f5f5;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14.32px;
+    margin-left: 18.04px;
+`;
+
+export const tip = styled.div``;
+
+export const tip_text = styled.div``;
+
+export const answer = styled.div``;
+
+export const doctor = styled.img``;
+
+export const answer_container = styled.div``;
+
+export const answer_text = styled.div``;
